a04156508cd3e74722d02d702dc4486d01547cb9
umitao <umitao@gmail.com>
2020-06-20 16:41:31 +0300

    Updates

---
+++ precache-manifest.e320f251b9d43bbfe1b022383a85bb7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fefc1a40ad87d6ffb04899b1a6e9f2",
+    "url": "/react-moviesapp/index.html"
+  },
+  {
+    "revision": "ba3469d6af38306b7674",
+    "url": "/react-moviesapp/static/css/main.fe4da3c5.chunk.css"
+  },
+  {
+    "revision": "fca5685ac832f7900b1e",
+    "url": "/react-moviesapp/static/js/2.d946a673.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-moviesapp/static/js/2.d946a673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3469d6af38306b7674",
+    "url": "/react-moviesapp/static/js/main.34b561fb.chunk.js"
+  },
+  {
+    "revision": "16a1d319bc579c41ae6b",
+    "url": "/react-moviesapp/static/js/runtime-main.557154cc.js"
+  }
+]);
